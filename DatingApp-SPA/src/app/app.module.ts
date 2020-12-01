2ed1d13e39b80b1dce434057a7d4707e12a9b9b0
Aleksandar Tomic <aleksandar.tomic@zoho.com>
2020-12-01 15:15:16 +0100

    End of section 15

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 export function tokenGetter() {
     return localStorage.getItem('token');
@@ -86,6 +87,7 @@ export function tokenGetter() {
     MemberDetailResolver,
     MemberListResolver,
     MemberEditResolver,
+    ListsResolver,
     PreventUnsavedChanges
   ],
   bootstrap: [AppComponent]
